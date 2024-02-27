c55369d827486fc91d796fb33016b2fb254df101
elflacoseba <sebaserri@gmail.com>
2024-02-27 13:02:02 -0300

    Implementación de un Guard para bloquear a un usuario a las rutas.

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,42 @@
+import { inject } from '@angular/core';
+import { CanMatchFn, Route, Router, UrlSegment } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+
+
+const checkAuthStatus = (): Observable<boolean> => {
+
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+
+  return authService.checkAuthentication()
+    .pipe(
+      tap(isAuthenticated => {
+        if (!isAuthenticated) {
+          router.navigate(['/auth/login']);
+        }
+
+      })
+    );
+
+}
+
+export const CanMatchGuard: CanMatchFn = (
+  route: Route,
+  segments: UrlSegment[]
+) => {
+
+  console.log( 'Can Match Guard' );
+  return true;
+}
+
+export const CantActivateGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+  ) => {
+
+    console.log( 'Can Activate Guard' );
+    return true;
+}
--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
@@ -8,10 +11,27 @@ import { Component } from '@angular/core';
 })
 export class LayoutPageComponent {
 
+  constructor(
+    private AuthService: AuthService,
+    private router: Router
+  ) { }
+
+  get user(): User | undefined {
+
+    return this.AuthService.currentUser;
+
+  }
+
   public sidebarItems = [
     { label: 'listado', icon: 'label', url: './list' },
     { label: 'Añadir', icon: 'add', url: './new-hero' },
     { label: 'Buscar', icon: 'search', url: './search' },
   ]
 
+  onLogout(): void {
+
+    this.AuthService.logout();
+    this.router.navigate(['/auth/login']);
+
+  }
 }
